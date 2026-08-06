629c9b6775096808aa073dfdf2cd1a9546faf61c
deczyk <deczjd@gmail.com>
2026-08-06 14:44:34 +0200

    Add realizacje placeholder page

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
         ofertaPremium: resolve(__dirname, 'oferta-premium.html'),
         ofertaDzierzawa: resolve(__dirname, 'oferta-dzierzawa.html'),
         oNas: resolve(__dirname, 'o-nas.html'),
+        realizacje: resolve(__dirname, 'realizacje.html'),
       },
     },
   },
